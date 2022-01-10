7251acbffd761975d69e402b7e433e8882857107
CelsonF <celson338540@gmail.com>
2022-01-09 21:59:47 -0300

    Add lessons 1 e 2

---
+++ Aula1/app.js
@@ -0,0 +1,11 @@
+//Tipos das variaveis 
+
+console.log(typeof 'Celson Fernando');
+
+console.log(typeof  123456);
+
+//Comentário de uma linha 
+
+/* Comentário 
+de várias
+linhas */
---
+++ Aula2/templateString.js
@@ -0,0 +1,5 @@
+//literal templates
+let nome = 'Celson Fernando'
+let dataAtual = new Date();
+
+console.log(`Meu nome é ${nome}. Estou aprendendo Java Script às ${dataAtual.getHours()}:${dataAtual.getMinutes()}`)
