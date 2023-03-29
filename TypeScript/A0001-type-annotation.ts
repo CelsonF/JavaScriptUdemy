3665720228b8329110a76ec87c37629582a9bb59
CelsonF <celsondev@outlook.com>
2023-03-29 00:15:10 -0300

    Add new class type-any

--- TypeScript/A0001-type-annotation.ts
+++ TypeScript/A0001-type-annotation.ts
@@ -5,3 +5,23 @@ let idade: number = 30;
 let adulto: boolean = true;
 let simbolo: symbol = Symbol('qualquer-symbol');
 let big: bigint = 10n
+
+//Arrays
+let arrayDeNumerosGenerico: Array<number> = [1,2,3];
+let arrayDeNumerosNaoGenerico: number[] = [1,2,3];
+let arrayDeStringGenerico: Array<String> = [`a`,`b`,`c`];
+let arrayDeStringNaoGenerico: string[] = [`a`,`b`,`c`];
+
+//Objects
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+	nome:'Celson',
+	idade: 25,
+	adulto: true
+}
+
+//Functions
+function sum(x: number, y: number): number{
+	return x + y;
+}
+
+const sumWithArraowFunction: (x: number, y: number) => number = (x, y) => x + y;
